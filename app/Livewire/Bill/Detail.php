c6424b45e47a7d7b09a82de334cfabc77cd450e7
dhaydt <1992dayat@gmail.com>
2024-01-10 20:04:43 +0700

    fix bill detail

--- app/Livewire/Bill/Detail.php
+++ app/Livewire/Bill/Detail.php
@@ -14,7 +14,7 @@ class Detail extends Component
     {
         $table = Helpers::getTableId();
 
-        $this->bill = Order::where(['table_id' => $table, 'on_going' => 1])->get();
+        $this->bill = Order::where(['table_id' => $table, 'on_going' => 1])->whereDate('created_at', date('Y-m-d'))->orderBy('created_at', 'desc')->get();
         
         return view('livewire.bill.detail');
     }

fdbad0334c77729221b0c761d1503001480e1a6d
dhaydt <1992dayat@gmail.com>
2024-01-14 18:27:43 +0700

    mb 80

--- resources/views/livewire/payment/index.blade.php
+++ resources/views/livewire/payment/index.blade.php
@@ -72,7 +72,7 @@
             </label>
         </div>
     </div>
-    <div class="next-wrapper confirm-btn mb-5 px-2 d-flex align-items-center justify-content-center" style="margin-bottom: 50px !important;">
+    <div class="next-wrapper confirm-btn mb-5 px-2 d-flex align-items-center justify-content-center" style="margin-bottom: 80px !important;">
         <a href="javascript:" wire:click="confirmPayment" class="next-btn">{{ $text }}</a>
     </div>
 
